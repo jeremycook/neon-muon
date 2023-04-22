cab7108c00e24fe7f565c5e45036756b8a104453
Jeremy Cook <jeremycook.co@gmail.com>
2023-04-21 20:23:59 -0600

    cp

--- Sqlil.Scratch/Program.cs
+++ Sqlil.Scratch/Program.cs
@@ -1,7 +1,9 @@
-﻿using Sqlil.Core.Syntax;
+﻿using Microsoft.Data.Sqlite;
+using Sqlil.Core.Syntax;
 using Sqlil.Scratch;
+using System.Data.Common;
 
-var translation = TranslationCases.Where;
+var translation = TranslationCases.SelectIdentity;
 // Console.WriteLine(translation);
 // Console.WriteLine();
 
@@ -11,18 +13,96 @@ var translation = TranslationCases.Where;
 SqliteComposer sqliteComposer = new();
 var parameterizedSql = sqliteComposer.Compose(translation);
 
-var i = 1;
-Console.WriteLine($@"Outputs: 
-  {string.Join(",\n  ", parameterizedSql.Segments.OfType<SqlOutput>())}
-Inputs:
-  {string.Join(",\n  ", parameterizedSql.Segments.OfType<SqlInput>())}
-Command Text:");
-Console.WriteLine("  " + string.Concat(parameterizedSql.Segments.Select(x => x switch {
+var sqlInputs = parameterizedSql.Segments.OfType<SqlInput>().ToArray();
+var sqlOutputs = parameterizedSql.Segments.OfType<SqlOutput>().ToArray();
+
+var parameterNumber = 1;
+var commandText = string.Concat(parameterizedSql.Segments.Select(x => x switch {
     SqlRaw raw => raw.Text,
-    SqlInput input => "@" + (input.SuggestedName != string.Empty ? input.SuggestedName : "p") + i++,
+    SqlInput input => "@" + (input.SuggestedName != string.Empty ? input.SuggestedName : "p") + parameterNumber++,
     SqlOutput output => string.Empty,
     _ => throw new NotSupportedException(x?.ToString())
-})).ReplaceLineEndings("\n  "));
+}));
+
+Console.WriteLine($@"Inputs: 
+  {string.Join(",\n  ", sqlInputs)}
+Outputs:
+  {string.Join(",\n  ", sqlOutputs)}
+Command Text:");
+Console.WriteLine("  " + commandText.ReplaceLineEndings("\n  "));
 Console.WriteLine();
 
-// System.Text.Json.JsonSerializer.Serialize(translation).Dump();
+using (DbConnection connection = new SqliteConnection(new SqliteConnectionStringBuilder() {
+    DataSource = "Scratch" + Random.Shared.Next(),
+    Mode = SqliteOpenMode.Memory,
+    Cache = SqliteCacheMode.Shared,
+}.ConnectionString)) {
+    connection.Open();
+
+    {
+        var ddl = connection.CreateCommand();
+        ddl.CommandText = """
+CREATE TABLE "User" (
+	"UserId"	INTEGER NOT NULL,
+	"Username"	TEXT NOT NULL DEFAULT 'JeremyCook' UNIQUE,
+	"IsActive"	INTEGER NOT NULL DEFAULT 1,
+	"Birthday"	TEXT,
+	"Created"	TEXT NOT NULL DEFAULT (datetime()),
+	PRIMARY KEY("UserId" AUTOINCREMENT)
+);
+
+INSERT INTO "User" DEFAULT VALUES;
+UPDATE "User" SET "Birthday" = '2023-04-21';
+""";
+        ddl.ExecuteNonQuery();
+    }
+
+    var cmd = connection.CreateCommand();
+    cmd.CommandText = commandText;
+    parameterNumber = 1;
+    cmd.Parameters.AddRange(sqlInputs
+        .Select(Input => new { Input, Param = cmd.CreateParameter() })
+        .Select(x => {
+            x.Param.ParameterName = (x.Input.SuggestedName != string.Empty ? x.Input.SuggestedName : "p") + parameterNumber++;
+            x.Param.Value = true;
+            return x.Param;
+        })
+        .ToArray());
+
+    using var reader = cmd.ExecuteReader();
+
+    var list = new List<object[]>();
+    while (reader.Read()) {
+        var values = new object[sqlOutputs.Length];
+        reader.GetValues(values);
+        for (int j = 0; j < values.Length; j++) {
+            var val = values[j];
+            var sqlOutput = sqlOutputs[j];
+            if (val == DBNull.Value) {
+                val = null!;
+            }
+            else if (val is string text) {
+                if (sqlOutput.Type.IsAssignableTo(typeof(Guid?))) {
+                    val = Guid.Parse(text);
+                }
+                else if (sqlOutput.Type.IsAssignableTo(typeof(DateOnly?))) {
+                    val = DateOnly.Parse(text);
+                }
+                else if (sqlOutput.Type.IsAssignableTo(typeof(DateTime?))) {
+                    var dt = DateTime.Parse(text);
+                    val = DateTime.SpecifyKind(dt, DateTimeKind.Utc);
+                }
+                else {
+                    // No change needed
+                }
+            }
+            else {
+                val = Convert.ChangeType(val, sqlOutputs[j].Type);
+            }
+            values[j] = val;
+        }
+        list.Add(values);
+    }
+    System.Text.Json.JsonSerializer.Serialize(list).Dump();
+}
+
--- Sqlil.Scratch/UserContext.cs
+++ Sqlil.Scratch/UserContext.cs
@@ -10,17 +10,11 @@ public readonly record struct Role(Guid RoleId, string Name) {
     public static Guid Admin { get; } = Guid.Parse("0ed5476c-8cb9-44a9-9db4-8dc45990d997");
     public static Guid New { get; } = Guid.Parse("456ecbd1-c5a9-4384-9a6e-e37dbefdb630");
 
-    public Role(string name) : this(Guid.NewGuid(), name) {
-        RoleId = Guid.NewGuid();
-        Name = name;
-    }
+    public Role(string name) : this(Guid.NewGuid(), name) { }
 }
 
-public readonly record struct User(Guid UserId, string Username, bool IsActive, DateTime Created, DateOnly? Birthday) {
-    public User(string username) : this(Guid.NewGuid(), username, true, DateTime.UtcNow, null) {
-        UserId = Guid.NewGuid();
-        Username = username;
-    }
+public readonly record struct User(long UserId, string Username, bool IsActive, DateTime Created, DateOnly? Birthday) {
+    public User(string username) : this(0, username, true, DateTime.UtcNow, null) { }
 }
 
-public readonly record struct UserRole(Guid UserId, Guid RoleId) { }
+public readonly record struct UserRole(long UserId, Guid RoleId) { }
