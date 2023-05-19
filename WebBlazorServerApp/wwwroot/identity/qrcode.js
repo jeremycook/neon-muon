6df0e7e46ee577414c213f78c2ea4064c7e3e83e
Jeremy Cook <jeremycook.co@gmail.com>
2023-05-19 05:30:29 -0600

    cp

--- WebBlazorServerApp/wwwroot/identity/qrcode.js
+++ WebBlazorServerApp/wwwroot/identity/qrcode.js
@@ -1,3 +1,4 @@
+// https://go.microsoft.com/fwlink/?Linkid=852423
 window.addEventListener("load", () => {
   const uri = document.getElementById("qrCodeData").getAttribute('data-url');
   new QRCode(document.getElementById("qrCode"),
