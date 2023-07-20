043268dd1a341b0188919853a6f0ab8b403b8bd5
Jeremy Cook <jeremycook.co@gmail.com>
2023-07-20 04:56:22 -0600

    cp

---
+++ WebHooks/HookSettings.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace WebHooks;
+
+public class HookSettings {
+    public string? WorkingDirectory { get; set; } = null!;
+    public string FileName { get; set; } = null!;
+    public HookAuthenticator Authenticator { get; set; }
+    public JsonDocument AuthenticatorSettings { get; set; } = null!;
+}
+
+public interface IHookAuthenticator {
+    Task<HttpStatusCode> Authenticate(HttpContext context, IConfigurationSection section);
+}
+
+public enum HookAuthenticator {
+    Unauthorized = 0,
+    GitHub = 1,
+}
+
+public class UnauthorizedAuthenticator : IHookAuthenticator {
+    public Task<HttpStatusCode> Authenticate(HttpContext context, IConfigurationSection section) {
+        return Task.FromResult(HttpStatusCode.Unauthorized);
+    }
+}
+
+public record GitHubAuthenticatorSettings(string Secret) { }
+public class GitHubAuthenticator : IHookAuthenticator {
+    public async Task<HttpStatusCode> Authenticate(HttpContext context, IConfigurationSection section) {
+        var secret = section.GetValue<string?>("Secret")
+            ?? throw new ArgumentException("Missing GitHub Authenticator Secret.", nameof(section));
+
+        var request = context.Request;
+
+        // Verify that the signature and body match
+        if (request.Headers.TryGetValue("X-Hub-Signature-256", out var providedSignature) &&
+            !string.IsNullOrWhiteSpace(providedSignature)
+        ) {
+            var key = Encoding.ASCII.GetBytes(secret);
+            var body = await ReadToEndAsync(request.Body);
+
+            using var hmac = new HMACSHA256(key);
+            var hashedBody = hmac.ComputeHash(body);
+
+            var signature = "sha256=" + string.Concat(Array.ConvertAll(hashedBody, x => x.ToString("x2")));
+
+            if (providedSignature != signature) {
+                return HttpStatusCode.Unauthorized;
+            }
+        }
+        else {
+            return HttpStatusCode.Unauthorized;
+        }
+
+        return HttpStatusCode.OK;
+    }
+
+    private static async Task<byte[]> ReadToEndAsync(Stream stream) {
+        if (stream is MemoryStream mem) {
+            return mem.ToArray();
+        }
+
+        using var memoryStream = new MemoryStream();
+        await stream.CopyToAsync(memoryStream);
+
+        return memoryStream.ToArray();
+    }
+}
--- WebHooks/Program.cs
+++ WebHooks/Program.cs
@@ -1,24 +1,36 @@
 using System.Diagnostics;
 using System.Text;
+using WebHooks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-var section = app.Configuration.GetSection("Hooks");
-if (!section.Exists()) {
+var sections = app.Configuration.GetSection("Hooks");
+if (!sections.Exists()) {
     throw new Exception("Missing Hooks section.");
 }
 
-var hooks = section.Get<Dictionary<string, string>>()!;
+foreach (var hookSection in sections.GetChildren()) {
+    var hook = hookSection.Get<HookSettings>()!;
+    app.Map("/" + hookSection.Key, async (HttpContext context) => {
 
-foreach (var hook in hooks) {
-    app.Map("/" + hook.Key, () => {
+        IHookAuthenticator authenticator = hook.Authenticator switch {
+            HookAuthenticator.Unauthorized => new UnauthorizedAuthenticator(),
+            HookAuthenticator.GitHub => new GitHubAuthenticator(),
+            _ => throw new NotSupportedException($"The {hook.Authenticator} hook authenticator is not supported."),
+        };
+        var authenticateResult = await authenticator.Authenticate(context, hookSection);
+        if (authenticateResult != System.Net.HttpStatusCode.OK) {
+            return Results.StatusCode((int)authenticateResult);
+        }
+
         var output = new StringBuilder();
         var error = new StringBuilder();
 
         using var p = new Process();
-        p.StartInfo.FileName = hook.Value;
+        p.StartInfo.WorkingDirectory = hook.WorkingDirectory;
+        p.StartInfo.FileName = hook.FileName;
         //p.StartInfo.Arguments = @"TODO";
         p.StartInfo.CreateNoWindow = true;
         p.StartInfo.RedirectStandardError = true;
@@ -31,11 +43,13 @@ foreach (var hook in hooks) {
         p.BeginOutputReadLine();
         p.WaitForExit();
 
-        if (error.Length > 0) {
-            return Results.BadRequest(error.ToString());
+        if (output.Length > 0) {
+            app.Logger.LogInformation("{HookName} std out: {StdOut}", hookSection.Key, output.ToString());
         }
-        else if (output.Length > 0) {
-            return Results.Content(output.ToString());
+
+        if (error.Length > 0) {
+            app.Logger.LogError("{HookName} std error: {StdError}", hookSection.Key, error.ToString());
+            return Results.BadRequest();
         }
         else {
             return Results.Ok();
