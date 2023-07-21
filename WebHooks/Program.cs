cc29fdf3f4bd77b3e139949aae5ef4e7e63870fc
Jeremy Cook <jeremycook.co@gmail.com>
2023-07-21 14:28:25 -0600

    cp

--- WebHooks/Program.cs
+++ WebHooks/Program.cs
@@ -11,6 +11,10 @@ foreach (var source in builder.Configuration.Sources.OfType<JsonConfigurationSou
     builder.Configuration.AddJsonFile(appSettings.GetFullPath(source.Path!), source.Optional, source.ReloadOnChange);
 }
 
+var jobQueue = new JobQueue();
+builder.Services.AddSingleton(jobQueue);
+builder.Services.AddHostedService<JobQueueHostedService>();
+
 var app = builder.Build();
 
 var hooks = app.Configuration.GetSection("Hooks");
@@ -28,40 +32,47 @@ foreach (var hook in hooks.GetChildren()) {
             HookAuthenticator.GitHub => new GitHubAuthenticator(),
             _ => throw new NotSupportedException($"The {hookSettings.Authenticator} hook authenticator is not supported."),
         };
+
         var authenticateResult = await authenticator.Authenticate(context, hook);
         if (authenticateResult != System.Net.HttpStatusCode.OK) {
+            app.Logger.LogWarning("{HookPath} authentication from {RemoteIp} failed.", hookPath, context.Connection.RemoteIpAddress);
             return Results.StatusCode((int)authenticateResult);
         }
 
-        var output = new StringBuilder();
-        var error = new StringBuilder();
+        app.Logger.LogInformation("{HookPath} authentication from {RemoteIp} succeeded.", hookPath, context.Connection.RemoteIpAddress);
 
-        using var p = new Process();
-        p.StartInfo.WorkingDirectory = hookSettings.WorkingDirectory;
-        p.StartInfo.FileName = hookSettings.FileName;
-        //p.StartInfo.Arguments = @"TODO";
-        p.StartInfo.CreateNoWindow = true;
-        p.StartInfo.RedirectStandardError = true;
-        p.StartInfo.RedirectStandardOutput = true;
-        p.StartInfo.RedirectStandardInput = false;
-        p.OutputDataReceived += (a, b) => output.Append(b.Data);
-        p.ErrorDataReceived += (a, b) => error.Append(b.Data);
-        p.Start();
-        p.BeginErrorReadLine();
-        p.BeginOutputReadLine();
-        p.WaitForExit();
+        jobQueue.EnqueueTask((_, _) => {
 
-        if (output.Length > 0) {
-            app.Logger.LogInformation("{HookName} std out: {StdOut}", hookPath, output.ToString());
-        }
+            var output = new StringBuilder();
+            var error = new StringBuilder();
 
-        if (error.Length > 0) {
-            app.Logger.LogError("{HookName} std error: {StdError}", hookPath, error.ToString());
-            return Results.BadRequest();
-        }
-        else {
-            return Results.Ok();
-        }
+            using var p = new Process();
+            p.StartInfo.WorkingDirectory = hookSettings.WorkingDirectory;
+            p.StartInfo.FileName = hookSettings.FileName;
+            //p.StartInfo.Arguments = @"TODO";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            p.OutputDataReceived += (a, b) => output.Append(b.Data);
+            p.ErrorDataReceived += (a, b) => error.Append(b.Data);
+            p.Start();
+            p.BeginErrorReadLine();
+            p.BeginOutputReadLine();
+            p.WaitForExit();
+
+            if (output.Length > 0) {
+                app.Logger.LogInformation("{HookPath} output: {StdOut}", hookPath, output.ToString());
+            }
+
+            if (error.Length > 0) {
+                app.Logger.LogError("{HookPath} error: {StdError}", hookPath, error.ToString());
+            }
+
+            return Task.CompletedTask;
+        });
+
+        return Results.Ok();
     });
 }
 
