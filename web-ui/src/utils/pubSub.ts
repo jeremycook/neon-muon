a316b55e3abd784d9ff99582f90175928de68535
Jeremy Cook <jeremycook.co@gmail.com>
2023-08-01 05:04:51 -0600

    cp

--- web-ui/src/utils/pubSub.ts
+++ web-ui/src/utils/pubSub.ts
@@ -8,52 +8,6 @@ export function computed<TInput, TValue>(value: PubSubT<TInput>, computation: ()
     return comp;
 }
 
-/** @deprecated Experimental */
-export function proxy<TValue extends object>(value: TValue): TValue & PubSub {
-
-    const _subscriptions: WeakRef<() => (void | Promise<void>)>[] = [];
-
-    function sub(lifetimeOwner: object, subscription: () => (void | Promise<void>)) {
-        _subscribe(_subscriptions, lifetimeOwner, subscription, 'proxy subscription');
-    }
-
-    async function pub() {
-        await _dispatch(_subscriptions);
-    }
-
-    const handler: ProxyHandler<TValue> = {
-        get(target, p, receiver) {
-            switch (p) {
-                case 'pub':
-                    return pub;
-
-                case 'sub':
-                    return sub;
-
-                default:
-                    return Reflect.get(target, p, receiver);
-            }
-        },
-        set(target, p, newValue, receiver) {
-            if ((target as any)[p] !== newValue) {
-                const result = Reflect.set(target, p, newValue, receiver);
-                pub();
-                return result;
-            }
-            else {
-                return false;
-            }
-        }
-        // apply(target, thisArg, argArray) {
-        //     console.log(target, thisArg, argArray)
-        //     return Reflect.apply(target, thisArg, argArray);
-        // },
-    };
-
-    const proxy1 = new Proxy(value, handler);
-    return proxy1 as any;
-}
-
 export interface Pub {
     pub(): Promise<void>;
 }
@@ -99,13 +53,13 @@ export class Val<TValue = unknown> implements PubSubT<TValue> {
     }
 }
 
-function _subscribe(subscriptions: WeakRef<() => (void | Promise<void>)>[], lifetimeOwner: object, subscription: () => (void | Promise<void>), description: string) {
+export function _subscribe(subscriptions: WeakRef<() => (void | Promise<void>)>[], lifetimeOwner: object, subscription: () => (void | Promise<void>), description: string) {
     const sym = Symbol(description);
     (lifetimeOwner as any)[sym] = subscription;
     subscriptions.push(new WeakRef(subscription));
 }
 
-async function _dispatch(subscriptions: WeakRef<() => (void | Promise<void>)>[]) {
+export async function _dispatch(subscriptions: WeakRef<() => (void | Promise<void>)>[]) {
     const gone: number[] = [];
     let i = -1;
     for (const ref of subscriptions) {
