4cd1e2b37db0aeedd182c103a7250d733de89877
Jeremy Cook <jeremycook.co@gmail.com>
2024-01-13 04:04:06 -0700

    Update vite.config.js

--- neonmoun-ui/vite.config.js
+++ neonmoun-ui/vite.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
-        basicSsl(),
+        // basicSsl(),
     ],
     server: {
-        port: 5174,
-        https: true,
+        port: 52236,
+        host: '127.0.0.1',
+        // https: true,
         proxy: {
             '/api': {
                 target: 'https://127.0.0.1:42236',
